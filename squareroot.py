72ee4eda8e31cdfd2cc1daac43a177d28b133571
charlottecode <charlottedesigner2013@gmail.com>
2025-02-19 20:18:26 -0500

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,12 @@
+
+number = float(input("Enter a number to calculate its square root: "))
+
+approximation = number / 2  
+
+approximation = (approximation + number / approximation) / 2
+approximation = (approximation + number / approximation) / 2
+approximation = (approximation + number / approximation) / 2
+approximation = (approximation + number / approximation) / 2
+approximation = (approximation + number / approximation) / 2
+
+print("The square root of (number) is approximately (approximation:.4")
